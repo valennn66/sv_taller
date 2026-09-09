28d6bda1d95bbb062dc2c8ddc091b2795321675c
valennn66 <valentinsoria777@gmail.com>
2026-09-09 02:13:55 -0300

    Implementacion de GET y POST en /tasks

--- server.py
+++ server.py
@@ -15,3 +15,19 @@ def read_json_body(environ):
     if not raw_body:
         return {}
     return json.loads(raw_body.decode('utf-8'))
+
+def app(environ, start_response):
+    global next_id
+    method = environ['REQUEST_METHOD']
+    path = environ['PATH_INFO']
+
+    if path == '/tasks':
+        if method == 'GET':
+            return response(start_response, '200 OK', tasks)
+
+    if method == 'POST':
+        new_task = read_json_body(environ)
+        new_task['id'] = next_id
+        next_id += 1
+        tasks.append(new_task)
+        return response(start_response, '201 Created', new_task)
